48f52713ee968838954811a0b732586b16b5bb3b
Ali Hasani <hasaninft1992@gmail.com>
2024-08-20 10:15:23 +0330

    chore(config): initialize project with basic setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Test from '@/components/Test'
+
+export default function Home() {
+  return (
+    <>
+      <div>landing page</div>
+      <Test value={1232} />
+    </>
+  )
+}
---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+  value: number
+}
+
+const Test = ({ value }: Props) => {
+  return <div>{value}</div>
+}
+
+export default Test
